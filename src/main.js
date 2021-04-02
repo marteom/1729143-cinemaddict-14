bd423d7da5e74d67263bb84daf27ce97c9677703
marteom <marteom@gmail.com>
2021-04-02 14:27:49 +0300

    add film-card view

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import { createProfileTemplate } from './view/profile';
 import { createMainMenuTemplate } from './view/site-menu';
 import { createSortContentTemplate } from './view/sort-content';
+import { createFilmCardTemplate } from './view/film-card';
 
 const render = (container, template, place) => {
     container.insertAdjacentHTML(place, template);
 };
 
+const FILM_COUNT = 5;
 const siteHeaderElement = document.querySelector('.header');
 const siteMainElement = document.querySelector('.main');
+const displayFilmList = siteMainElement.querySelector('.films');
 
 render(siteHeaderElement, createProfileTemplate(), 'beforeend');
 render(siteMainElement, createMainMenuTemplate(), 'beforeend');
