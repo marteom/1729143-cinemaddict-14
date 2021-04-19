192ba72dc5a67fb7db00ffb7d42775594b736b7f
marteom <marteom@gmail.com>
2021-04-19 17:31:49 +0300

    add close on esc and empty message

---
+++ src/view/films-list-empty.js
@@ -0,0 +1,27 @@
+import {createElement} from '../helpers/utils';
+
+const createFilmsListEmptyTemplate = () => {
+  return '<h2 class="films-list__title">There are no movies in our database</h2>';
+};
+
+export default class FilmsListEmpty {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createFilmsListEmptyTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
